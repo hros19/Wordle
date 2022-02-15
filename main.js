0f4143b7c4dc94f3a9175dd5a885281cdfd4fa06
Hansol A. Rostrán <89949216+hros19@users.noreply.github.com>
2022-02-14 20:58:54 -0600

    Proyecto completo.
    Implementando estadisticas!.

--- main.js
+++ main.js
@@ -24,18 +24,18 @@ var palabraElegida = "";
 //Cuadrado actual
 var indCuadrado = 1;
 
+//Estadisticas <<<<
+var totalJugadas = 0;
+var totalPerdidas = 0;
+var partidasGanadas = [0, 0, 0, 0, 0, 0];
+var rachaActual = 0;
+var mejorRacha = 0;
+
 //Botones del teclado
 const teclas = document.querySelectorAll('.teclado button')
 
 //Fila actual
 var filaActual = 0;
-
-/*Borrar en entrega
-let mostrarObj = function() {
-  for (let carac in objPrincipal) {
-    console.log(objPrincipal[carac]);
-  };
-}*/
 
 fetch("./lista_palabras.json")
     .then(function(resp) {
@@ -44,11 +44,14 @@ fetch("./lista_palabras.json")
     .then(function(data) {
       objPrincipal = data;
       elegirPalabra();
+      verStats();
     });
 
 function reiniciarJuego() {
   letrasElegidas = [];
   elegirPalabra();
+  reiniciarTeclado();
+  reiniciarTablero();
   indCuadrado = 1;
   filaActual = 0;
 }
@@ -67,6 +70,18 @@ function letra(letter, id) {
   cuadradoActual.textContent = letter;
   cuadradoActual.backgroundColor = "#1f1f1f";
   letrasElegidas.push(id);
+}
+
+function ocultarStats() {
+  var ventanaStats = document.getElementById("modal-container");
+  ventanaStats.style.opacity = "0";
+  ventanaStats.style.pointerEvents = "none";
+}
+
+function mostrarStats() {
+  var ventanaStats = document.getElementById("modal-container");
+  ventanaStats.style.opacity = "100";
+  ventanaStats.style.pointerEvents = "auto";
 }
 
 function teclaFuncional(nombre) {
@@ -94,9 +109,12 @@ function teclaFuncional(nombre) {
   }
 }
 
-function reiniciarJuego() {
-  window.alert("Felicidades, has ganado!")
+function verStats() {
+  var parag = document.getElementById("statsParag");
+
+  parag.innerHTML = statsToString();
 }
+
 function verificarCampos() {
   var palabraDigitada = formarPalabra();
 
@@ -104,8 +122,6 @@ function verificarCampos() {
 
   //Jugador termino y gano
   if (palabraDigitada == palabraElegida) {
-    desactivarTeclado();
-    window.alert("Felicidades, has ganado!");
     for (let i in letrasElegidas) {
       var letra = document.getElementById(letrasElegidas[i]);
       letra.style.backgroundColor = 'green';
@@ -116,125 +132,158 @@ function verificarCampos() {
       cuadrado.style.backgroundColor = 'green';
       cuadrado.style.borderColor = 'white';
     }
+    actualizarStats("gano");
+    window.alert("Felicidades, has ganado!");
+    setTimeout(() => { reiniciarJuego(); }, 2000);
     return;
   }
 
   var posIguales = getPosIguales(); //Son letras que esta en la posicion correcta.
   var posRotables = getPosRotables(); //Rotable es que pertenece a la palabra pero no en esa posicion.
   var totalElemCambiar = posIguales.length + posRotables.length;
-
-  //Colorear posiciones rotables
-  if (posRotables.length != 0) {
-    for (let i = 0; i < 5; i++) {
-      if (posRotables.indexOf(i) != -1) {
-        totalElemCambiar--;
-        var letra = document.getElementById(letrasElegidas[i]);
-        letra.style.backgroundColor = 'orange';
-        letra.style.borderColor = 'white';
-
-        var cuadrado = document.getElementById(i + 1 + (5 * filaActual));
-        cuadrado.style.backgroundColor = 'orange';
-        cuadrado.style.borderColor = 'white';
-      } else {
-        if (posRotables.indexOf(i) != -1) {
-          continue;
-        }
-        var letra = document.getElementById(letrasElegidas[i]);
-        if (posIguales.indexOf(i) == -1 && palabraElegida.indexOf(letra.textContent.charAt(7)) == -1) {
-          var cuadrado = document.getElementById(i + 1 + (5 * filaActual));
-          cuadrado.style.backgroundColor = '#252525';
-          cuadrado.style.borderColor = '#252525';
-
-          var letra = document.getElementById(letrasElegidas[i]);
-          letra.style.backgroundColor = '#252525';
-          letra.style.borderColor = '#252525';
-          letra.disabled = true;
-        } else {
-          var cuadrado = document.getElementById(i + 1 + (5 * filaActual));
-          cuadrado.style.backgroundColor = '#252525';
-          cuadrado.style.borderColor = '#252525';
-        }
-      }
-    }
-    flagPalabraNull = false;
-  }
+  var posRestantes = [0, 1, 2, 3 ,4];
 
   //Colorear posiciones iguales
   if (posIguales.length != 0) {
     for (let i = 0; i < 5; i++) {
       if (posIguales.indexOf(i) != -1) {
         totalElemCambiar--;
+        posRestantes = arrayRemove(posRestantes, i);
         var letra = document.getElementById(letrasElegidas[i]);
         letra.style.backgroundColor = 'green';
         letra.style.borderColor = 'white';
-        letra.disabled = false;
 
         var cuadrado = document.getElementById(i + 1 + (5 * filaActual));
         cuadrado.style.backgroundColor = 'green';
         cuadrado.style.borderColor = 'white';
-      } else {
-        for (let i = 0; i < 5; i++) {
-          var letra = document.getElementById(letrasElegidas[i]);
-          if (palabraElegida.indexOf(letra.textContent.charAt(7)) == -1) {
-            letra.style.backgroundColor = '#252525';
-            letra.style.borderColor = '#252525';
-            letra.disabled = true;
-
-            var cuadrado = document.getElementById(i + 1 + (5 * filaActual));
-            cuadrado.style.backgroundColor = '#252525';
-            cuadrado.style.borderColor = '#252525';
-          } else {
-            if (totalElemCambiar == 0) {
-              var cuadrado = document.getElementById(i + 1 + (5 * filaActual));
-              cuadrado.style.backgroundColor = '#252525';
-              cuadrado.style.borderColor = '#252525';
-            }
-          }
-        }
-        flagPalabraNull = false;
-      } // fin else
-    }
-    for (let i = 0; i < 5; i++) {
-      if (posIguales.indexOf(i) == -1 && posRotables.indexOf(i) == -1) {
-        var cuadrado = document.getElementById(i + 1 + (5 * filaActual));
-        cuadrado.style.backgroundColor = '#252525';
-        cuadrado.style.borderColor = '#252525';
       }
     }
-    flagPalabraNull = false;
   }
+  console.log(totalElemCambiar);
 
-  if (flagPalabraNull == false) {
-    avanzarFila();
-    return;
-  }
-
-  //Son completamente diferentes
-  else {
+  //Colorear posiciones rotables
+  if (posRotables.length != 0) {
     for (let i = 0; i < 5; i++) {
-      var letra = document.getElementById(letrasElegidas[i]);
-      letra.style.backgroundColor = '#252525';
-      letra.style.borderColor = '#252525';
-      letra.disabled = true;
+      if (posRotables.indexOf(i) != -1) {
+        var letra = document.getElementById(letrasElegidas[i]);
+        var cuadrado = document.getElementById(i + 1 + (5 * filaActual));
+        if (cuadrado.style.backgroundColor != 'green') {
+          letra.style.backgroundColor = 'orange';
+          letra.style.borderColor = 'white';
 
+          cuadrado.style.backgroundColor = 'orange';
+          cuadrado.style.borderColor = 'white';
+          posRestantes = arrayRemove(posRestantes, i);
+          totalElemCambiar--;
+        }
+      }
+    }
+  }
+
+  //No pertenecen a la palabra
+  for (let i = 0; i < 5; i++) {
+    if (posRestantes.indexOf(i) != -1) {
+      var letra = document.getElementById(letrasElegidas[i]);
       var cuadrado = document.getElementById(i + 1 + (5 * filaActual));
-      cuadrado.style.backgroundColor = '#252525';
-      cuadrado.style.borderColor = '#252525';
+      
+      if (palabraElegida.indexOf(letra.textContent.charAt(7)) == -1) {
+        letra.disabled = true;
+        letra.style.backgroundColor = "#252525";
+        letra.style.borderColor = "#252525";
+      }
+
+      cuadrado.style.backgroundColor = "#252525";
+      cuadrado.style.borderColor = "#252525";
     }
   }
   avanzarFila();
-  window.alert(palabraElegida + " / " + palabraDigitada);
+  return;
+}
+
+function actualizarStats(estado) {
+  totalJugadas++;
+
+  if (rachaActual > mejorRacha) {
+    mejorRacha = rachaActual;
+  }
+
+  if (estado == "perdio") {
+    totalPerdidas++;
+    rachaActual = 0;
+  } else {
+    calcularGanadas();
+  }
+}
+
+function calcularGanadas() {
+  switch (filaActual) {
+    case 0: //a la primera
+      partidasGanadas[0]++;
+      break;
+    case 1: //a la segunda
+      partidasGanadas[1]++;
+      break;
+    case 2: //a la tercera
+      partidasGanadas[2]++;
+      break;
+    case 3: //a la cuarta
+      partidasGanadas[3]++;
+      break;
+    case 4: //a la quinta
+      partidasGanadas[4]++;
+      break;
+    case 5: //apenas
+      partidasGanadas[5]++;
+  }
+}
+
+function arrayRemove(arr, value) {
+  return arr.filter(function(ele) {
+    return ele != value;
+  });
+}
+
+function sumOfArray(arr) {
+  var result = 0;
+  for (let i in arr) {
+    result += arr[i];
+  }
+  return result;
 }
 
 function avanzarFila() {
   if (filaActual == 5) {
     //Termino el juego.
+    actualizarStats("perdio");
     desactivarTeclado();
     window.alert("La respuesta es: " + palabraElegida + ". Mejor suerte a la proxima!");
+    reiniciarJuego();
     return;
   }
   filaActual++;
   letrasElegidas = [];
+}
+
+function statsToString() {
+  let stats = "";
+  var totalGanadas = totalJugadas - totalPerdidas;
+  var winrate = Math.round((totalGanadas/totalJugadas)*100);
+
+  stats += "Partidas Jugadas: " + totalJugadas + '<br/>';
+  stats += "Partidas Ganadas: " + totalGanadas + '<br/>';
+  stats += "Partidas Perdidas: " + totalPerdidas + '<br/>';
+  stats += "Porcentaje de victorias: " + winrate + "%" + '<br/>';
+  stats += "Racha actual: " + rachaActual + '<br/>';
+  stats += "Mejor racha: " + mejorRacha + '<br/><br/>';
+  stats += "Victorias segun cantidad de intentos: " + '<br/>';
+  stats += "Un intento: " + partidasGanadas[0] + '<br/>';
+  stats += "Dos intentos: " + partidasGanadas[1] + '<br/>';
+  stats += "Tres intentos: " + partidasGanadas[2] + '<br/>';
+  stats += "Cuatro intentos: " + partidasGanadas[3] + '<br/>';
+  stats += "Cinco intentos: " + partidasGanadas[4] + '<br/>';
+  stats += "Apenas: " + partidasGanadas[5] + '<br/>';
+
+  return stats;
 }
 
 function getPosRotables() {
@@ -317,14 +366,28 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-/*
+function reiniciarTablero() {
+  for (let i = 1; i < 31; i++) {
+    var cuadrado = document.getElementById(i);
+    cuadrado.style.borderColor = "#818181";
+    cuadrado.style.background = "#000000";
+    cuadrado.textContent = "";
+  }
+}
+
 function reiniciarTeclado() {
-  document.getElementById("enter").disabled = false;
-  document.getElementById("backspace").disabled = false;
+  var enter = document.getElementById("enter")
+  var backspace = document.getElementById("backspace")
   for (let i = 0; i < teclas.length; i++) {
     teclas[i].disabled = false;
+    teclas[i].style.backgroundColor = "#504f4f";
+    teclas[i].style.borderColor = "#797979";
   }
-} */
+  enter.style.background = "#f14519";
+  enter.style.borderColor = "#eee";
+  backspace.style.background = "#f14519";
+  backspace.style.borderColor = "#eee";
+} 
 
 function desactivarTeclado() {
   document.getElementById("enter").disabled = true;
